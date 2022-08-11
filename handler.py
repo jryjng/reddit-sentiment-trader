d2c5e71a5762452559046f2814934413da7bff51
jryjng <shadowspirit56@gmail.com>
2022-08-10 23:24:16 -0500

    v1

--- handler.py
+++ handler.py
@@ -2,16 +2,23 @@ import alpaca_trade_api as alpaca
 import requests, yfinance
 import math, json, os, datetime
 
-# TODO: max out equity percentage 
-# TODO: multiday calling
+# TODO: max out equity percentage (partially implemented)
+# TODO: multiday calling 
 # TODO: add crypto support
-# TODO: potential stock buyback
-# TODO: factor in sentiment
+# TODO: analytics
 
-# Import global variables
-ALPACA_API_KEY = os.environ["ALPACA_API_KEY"]
-ALPACA_SECRET_KEY = os.environ["ALPACA_SECRET_KEY"]
-BASE_URL = 'https://paper-api.alpaca.markets'
+# Constants. These constants are NOT used in getStockVal()
+MIN_MENTIONS = 25
+MIN_PURCHASE_PERCENT = 0.05
+MIN_PURCHASE_RAW = 2500
+PLPC_SELL = -0.1
+MENTION_GROWTH_SELL = 0
+PRICE_GROWTH_SELL = -0.1
+MIN_SENTIMENT = 0.5
+MAX_POSITION_PERCENT = 0.5
+
+# Blacklisted stocks
+boomerStocks = ["SPY", "QQQ", "TQQQ", "UVXY", "SQQQ"]
 
 
 # https://github.com/alpacahq/alpaca-trade-api-python/
@@ -38,7 +45,7 @@ def getStockVal(mentions, mention_growth, stock_change):
 
 # Sell all of a position
 def sellPosition(alpaca_api, position, debugFlag):
-    print("========= Selling", position.symbol + ".", "Change%:", position.unrealized_plpc, "=========")
+    print("Sold "  + position.symbol + "; Profit:", (position.unrealized_plpc))
     
     if debugFlag:
         return 0
@@ -55,7 +62,8 @@ def sellPosition(alpaca_api, position, debugFlag):
 
 # Buy a position
 def buyPosition(alpaca_api, amount, apeInfo, debugFlag):
-    print("========= Buying", amount, "shares of", apeInfo["ticker"],  "for a total of $", round(amount*apeInfo["price"], 3), "=========")
+    print("Buying " + apeInfo["ticker"] + ":", amount, "shares;", "$" + str(round(amount*apeInfo["price"], 3)))
+
     if debugFlag:
         return 0
 
@@ -69,30 +77,30 @@ def buyPosition(alpaca_api, amount, apeInfo, debugFlag):
     return 1
 
 
-# Return an ape's most important attributes as a dict
+# Return an ape object: name, price, mentions, mentions_growth, price_growth
+# Percentages are fractional values i.e. 100%=1
+# Returns 0 on error
 def apeFactory(ape):
     try:
-        # apewisdom uses BTC.X
-        # yfinance uses BTC-USD
-        # alpaca api uses BTC/USD
+        # Name
         name = ape["ticker"]
 
-        # Retrieve relevant data
+        # Mentions
         mentions = float(ape["mentions"])
 
+        # Mentions Growth
         # Account for new stock - set to 500%
         mentions_growth = 5
         if ape["mentions_24h_ago"] != None:
             mentions_growth = (mentions - 
             float(ape["mentions_24h_ago"]))/float(ape["mentions_24h_ago"])
 
-        # Get price change
-        # Note: history and download gets the last trading day
+        # Price Change and current price
         ape_ticker = yfinance.Ticker(name)
         prev_close = ape_ticker.history(period="2d").reset_index().loc[0, "Close"]
 
         # The current price has a 5m day
-        df = yfinance.download(tickers=name, period='1d', interval='1m').reset_index()
+        df = yfinance.download(tickers=name, period='1d', interval='1m', progress=False).reset_index()
         price = df.tail(1).reset_index().loc[0, "Close"]
         price_growth = (price/prev_close) - 1
         return {
@@ -103,7 +111,7 @@ def apeFactory(ape):
             "price_growth" : price_growth
         }
     except:
-        print("apeFactory: Stock not supported")
+        print("ERROR: Stock not supported (apefactory)")
         return 0
 
 
@@ -122,34 +130,38 @@ def roundStockPrice(alpaca_api, ticker, stockPrice, money):
             # Round down
             return int(money / stockPrice)
     except:
-        print("alpaca-roundStock: Stock not supported")
+        print("ERROR: Stock not supported (roundstock)")
         return 0    
 
 
 # Stock Blacklist
 def boomer(stock):
-    boomerStocks = ["SPY", "QQQ"]
     return stock in boomerStocks;
 
 
-def main(event, context):
-    # Get the first page of ape wisdom using their API
-    response = requests.get("https://apewisdom.io/api/v1.0/filter/all-stocks/page/1")
-    response.raise_for_status()
+# Get the sentiment of the stock using apewisdom
+# Return [0, 1] on success. Higher to 1 indicating stronger sentiment
+# Return -1 on error. Relies on web scraping so this data can be unreliable
+def getSentiment(ticker):
+    try:
+        html = requests.get("https://apewisdom.io/stocks/" + ticker).text
+        # Pretty poor way of doing this but it works for now. TODO implement using bs4
+        search_str = '<div style="float:left; padding-left: 10px; padding-top: 2px; font-weight:bold; font-size:18px;">';
+        start = html.index(search_str)
+        if start == -1:
+            return -1
+        end = html.index("%", start + len(search_str))
+        result = float(html[start+len(search_str):end].strip())/100
+        print(ticker + " sentiment:", result)
+        return result
+    except:
+        print("ERROR: Could not access sentiment value")
+        return -1
 
-    # Load the JSON into python dict
-    ape_list = json.loads(response.content)["results"]
 
-    # Connect to alpaca
-    alpaca_api = alpaca.REST(
-        ALPACA_API_KEY, ALPACA_SECRET_KEY, base_url=BASE_URL)
-
-    # Retrieve account info
-    account = alpaca_api.get_account()
+# Sell
+def sellRoutine(alpaca_api, account, ape_list, debugFlag):
     positions = alpaca_api.list_positions()
-
-    # Flag used to disable trading activity
-    debugFlag = True
 
     # Try selling
     for position in positions:
@@ -161,31 +173,42 @@ def main(event, context):
                 break
         
         # DEBUG: print
-        if apeInfo == None:
-            print("Missing apeinfo!")
-        else:
-            print(apeInfo["ticker"], apeInfo["mentions_growth"], apeInfo["price_growth"],
-                position.unrealized_plpc)
+        if apeInfo != None:
+            print("Selling Analysis:", apeInfo["ticker"], apeInfo["mentions_growth"], apeInfo["price_growth"],
+                position.unrealized_plpc, position.market_value)
 
         # Evaluate if should sell or not
+        # If sell, then print the reason, blacklist the stock, and sell it
         if apeInfo == None:
+            print("Sell reason: No apeinfo")
+            boomerStocks.append(apeInfo["ticker"])
             sellPosition(alpaca_api, position, debugFlag)
-        elif apeInfo["mentions_growth"] <= 0:
+        elif apeInfo["mentions_growth"] <= MENTION_GROWTH_SELL:
+            print("Sell reason: Decline in mentions")
+            boomerStocks.append(apeInfo["ticker"])
             sellPosition(alpaca_api, position, debugFlag)
-        elif apeInfo["price_growth"] < -10:
+        elif apeInfo["price_growth"] < PRICE_GROWTH_SELL:
+            print("Sell reason: Recent price decline")
+            boomerStocks.append(apeInfo["ticker"])
             sellPosition(alpaca_api, position, debugFlag)
-        elif float(position.unrealized_plpc) < -0.1:
-            # TODO: find plpc meaning
+        elif float(position.unrealized_plpc) < PLPC_SELL:
+            print("Sell reason: Unprofitability")
+            boomerStocks.append(apeInfo["ticker"])
             sellPosition(alpaca_api, position, debugFlag)
+        else:
+            # Check stock relative to portfolio value
+            position_percent = float(position.market_value) / (float(account.multiplier) * float(account.equity))
+            print(apeInfo["ticker"] + " BP allocation: " + str(position_percent) + "%")
+            if position_percent > MAX_POSITION_PERCENT:
+                boomerStocks.append(apeInfo["ticker"])
 
-
-    # DEBUG: print
-    print("Available Assests %", float(account.buying_power) / float(account.equity))
-
+# Buy
+def buyRoutine(alpaca_api, ape_list, account, debugFlag):
     # Try buying
     for ape in ape_list:
         # Don't buy boring boomer stocks
         if boomer(ape["ticker"]):
+            print("Skipping:", ape["ticker"])
             continue;
 
         apeInfo = apeFactory(ape)
@@ -195,7 +218,7 @@ def main(event, context):
             continue;
 
         # Stock if mentions falls below threshold
-        if apeInfo["mentions"] < 25:
+        if apeInfo["mentions"] < MIN_MENTIONS:
             print("Stopping at", apeInfo["ticker"])
             break
 
@@ -205,18 +228,54 @@ def main(event, context):
         apeInfo["price_growth"])
 
         # DEBUG: print
-        print(ape["ticker"], apeInfo["mentions"], apeInfo["mentions_growth"], 
+        print("Buy analysis:", ape["ticker"], apeInfo["mentions"], apeInfo["mentions_growth"], 
         apeInfo["price_growth"], stockVal)
 
         # Think about buying $rawBuying amount of the assest
         if stockVal > 0:
             stocks_to_buy = roundStockPrice(alpaca_api, apeInfo["ticker"], apeInfo["price"], 
             stockVal * float(account.buying_power))
-            
             rawAmount = stocks_to_buy * apeInfo["price"]
-            # Don't buy too little. Buy only if 5% of equity or exceeds 10k
-            if rawAmount > 0.05 * float(account.equity) or rawAmount > 10000:
-                buyPosition(alpaca_api, stocks_to_buy, apeInfo, debugFlag)
 
-    # Successful completion?            
+            # Minimum buying amount
+            if rawAmount > MIN_PURCHASE_PERCENT * (float(account.multiplier) * float(account.equity)) or rawAmount > MIN_PURCHASE_RAW:
+                # Last check for sentiment
+                # Sentiment is only checked here to minimize web scraping
+                # Ignore errors because of unreliability
+                sentiment = getSentiment(apeInfo["ticker"])
+                if sentiment < MIN_SENTIMENT and sentiment != -1:
+                    continue;
+                else:
+                    buyPosition(alpaca_api, stocks_to_buy, apeInfo, debugFlag)
+
+
+# Main routine
+def apeAlgorithm(ALPACA_API_KEY, ALPACA_SECRET_KEY, debugFlag):
+    # Get the first page of ape wisdom using their API
+    response = requests.get("https://apewisdom.io/api/v1.0/filter/all-stocks/page/1")
+    response.raise_for_status()
+
+    # Load the JSON into python dict
+    ape_list = json.loads(response.content)["results"]
+
+    # Connect to alpaca on paper markets
+    alpaca_api = alpaca.REST(
+        ALPACA_API_KEY, ALPACA_SECRET_KEY, 'https://paper-api.alpaca.markets')
+    account = alpaca_api.get_account()
+
+    # Sell
+    sellRoutine(alpaca_api, account, ape_list, debugFlag)
+
+    # DEBUG: print
+    print("Available Assets:", str(float(account.buying_power) / (float(account.multiplier) * float(account.equity))*100)+"%")
+
+    # Buy
+    buyRoutine(alpaca_api, ape_list, account, debugFlag)
+
+    # Successful completion?
     return 200
+
+
+# Gets called by AWS
+def main(event, context):
+    return apeAlgorithm(os.environ["ALPACA_API_KEY"], os.environ["ALPACA_SECRET_KEY"], False)
